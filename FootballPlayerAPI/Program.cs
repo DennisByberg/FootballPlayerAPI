7432cb721c17d78f32535d813f6e8051f89b773a
Dennis Byberg <dennis.byberg@hotmail.com>
2025-02-19 17:43:13 +0100

    Added secrets

--- FootballPlayerAPI/Program.cs
+++ FootballPlayerAPI/Program.cs
@@ -3,16 +3,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Load environment variables from .env file
+DotNetEnv.Env.Load();
 
+// Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Construct the connection string from environment variables
+var server = Environment.GetEnvironmentVariable("DB_SERVER");
+var user = Environment.GetEnvironmentVariable("DB_USER");
+var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+var connectionString = $"Server=tcp:{server}.database.windows.net,1433;Initial Catalog=FootballPlayerDB;Persist Security Info=False;User ID={user};Password={password};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
 builder.Services.AddDbContext<DataContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseSqlServer(connectionString);
 });
 
 var app = builder.Build();
